0b94bb5ab5b3ec403f10d667f774821bf5752312
ryglo1337 <115521236+ryglo1337@users.noreply.github.com>
2022-12-28 08:37:54 +0300

    Add files via upload

---
+++ web5/main.js
@@ -0,0 +1,40 @@
+window.addEventListener("DOMContentLoaded", function (even) {
+
+    console.log("DOM fully loaded and parsed");
+
+    let b = document.getElementById("click");
+
+    b.addEventListener("click", calculate);
+
+});
+
+function calculate() {
+  let firstProduct = Number(document.getElementById('firstProduct').value);
+  let firstLot = Number(document.getElementById('firstLot').value);
+  let secondProduct = Number(document.getElementById('secondProduct').value);
+  let secondLot = Number(document.getElementById('secondLot').value);
+
+  let re = new RegExp(/^[0-9]+$/i);
+  if (
+    !(
+      re.test(firstProduct) &&
+      re.test(firstLot) &&
+      re.test(secondProduct) &&
+      re.test(secondLot)
+    )
+  ) {
+    alert('Нужно писать число!');
+  } else if (firstProduct == '') {
+    alert('вы не указали цену 1 товару');
+  } else if (firstLot == '') {
+    alert('вы не указали кол-во 1 товара');
+  } else if (secondProduct == '') {
+    alert('вы не указали цену 2 товару');
+  } else if (secondLot == '') {
+    alert('вы не указали кол-во 2 товара');
+  } else {
+    const cost = firstProduct * firstLot + secondProduct * secondLot;
+    document.getElementById('cost').innerHTML =
+      'Стоимость равна: ' + cost + ' Руб.';
+  }
+}
